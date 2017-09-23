d8fb47c6afd3b6f31438a2890ee36fb265d2fa19
Vishesh <visheshsingh16@gmail.com>
2017-09-22 22:03:10 -0400

    JSX expressions and Conditional rendering

--- js/app.js
+++ js/app.js
@@ -1,10 +1,65 @@
 'use strict';
 
 //JSX
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life decisions in the hands of a computer',
+    options: ['one', 'two']
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'This is JSX from react.js'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    )
+);
+
+//Conditional Rendering example
+var user = {
+    name: 'Vish',
+    age: 26,
+    location: 'New York'
+};
+
+function geoLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    geoLocation(user.location)
 );
 var appRoot = document.getElementById('app');
 
--- src/react.js
+++ src/react.js
@@ -1,5 +1,38 @@
 //JSX
-var template = <h1>This is JSX from react.js</h1>;
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life decisions in the hands of a computer',
+    options: ['one', 'two']
+}
+
+var template = (
+<div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+</div>
+);
+
+//Conditional Rendering example
+var user = {
+    name: 'Vish',
+    age: 26,
+    location: 'New York'
+}
+
+function geoLocation(location){
+    if(location){
+        return <p>Location: {location}</p>;
+    }
+}
+
+var template2 = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        {geoLocation(user.location)}
+    </div>
+    );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template,appRoot);
+ReactDOM.render(template, appRoot);
