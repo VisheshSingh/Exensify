8a18bd5c628b00ca9d0f1cacb20f3df7f6fac945
Vishesh <visheshsingh16@gmail.com>
2017-09-29 21:58:06 -0400

    install,import and use npm modules inside webpack

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
-import './utils.js';
+/* import './utils.js';
 import subtract,{square, add} from './utils.js';
 
 console.log('app.js is running!');
 console.log(square(5));
 console.log(add(541,123));
-console.log(subtract(10,41));
+console.log(subtract(10,41)); */
 
 /* import { isAdult, canDrink } from './person.js';
 
 console.log(isAdult(31));
 console.log(canDrink(12)); */
+
+//install ->  import -> use npm modules
+/* import validator from 'validator';
+
+console.log(validator.isEmail('test@gmail.com')); */
+
+//import, install and use react and react-dom npm modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'Test123');
+ReactDOM.render(template, document.getElementById('app'));
