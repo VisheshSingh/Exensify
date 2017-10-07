eb78452fb4da40047cd9c48a1f6f7c8faa8ee47d
Vishesh <visheshsingh16@gmail.com>
2017-10-06 23:53:24 -0400

    Query Strings and URL parameters

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const EditExpense = () => (
-    <div>This is Edit expense component</div>
-);
+const EditExpense = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editing the expense with id: {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpense;
