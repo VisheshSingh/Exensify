6f5c9fe6401e875dd99ce205c0116038efe1d06d
Vishesh <visheshsingh16@gmail.com>
2017-10-07 20:11:10 -0400

    ES6 Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -15,16 +15,26 @@
 //     console.log(`It is ${temparature} in ${city}`);
 // }
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const {name: publishername = "self-published"} = book.publisher;
-if(publishername) {
-    console.log(`Name of the publisher is ${publishername}`)
-}
+// const {name: publishername = "self-published"} = book.publisher;
+// if(publishername) {
+//     console.log(`Name of the publisher is ${publishername}`)
+// }
+
+//ARRAY DESTRUCTURING
+
+const address = ['3 prospect st', 'Princeton', 'NY', '14234'];
+const [, city, state] = address;
+console.log(`You are in ${city}, ${state}`);
+
+const item = ['coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [name, , med] = item;
+console.log(`A medium ${name} is ${med}`); 
 
